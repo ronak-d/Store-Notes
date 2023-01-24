1da3083133e0032ad2c6ffab89297a0b88c4b0f0
ronak-d <ronakdodeja73@gamil.com>
2023-01-24 18:41:20 +0530

    done with the accordian all working properly

--- frontend/src/Pages/MyNotes/MyNotes.jsx
+++ frontend/src/Pages/MyNotes/MyNotes.jsx
@@ -7,12 +7,15 @@ import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 import axios from "axios";
 import { useEffect } from "react";
+import Accordion from "react-bootstrap/Accordion";
 
 const MyNotes = () => {
   const [notes, setNotes] = useState(null);
 
   function deleteHandler(id) {
-    if (window.confirm("Are you sure you want to delete this note"));
+    if (
+      window.confirm(`Are you sure you want to delete this note has id : ${id}`)
+    );
   }
 
   const fetchNotes = async () => {
@@ -33,7 +36,7 @@ const MyNotes = () => {
   return (
     <>
       <MainScreen title={"welcome back my son..."}>
-        <Button variant="primary">
+        <Button style={{ margin: "20px" }} variant="primary">
           <Link
             style={{ textDecoration: "none", color: "white" }}
             to="/createnotes"
@@ -41,59 +44,65 @@ const MyNotes = () => {
             Create New Note
           </Link>
         </Button>
+
         {notes?.map((note, i) => (
-          <Card key={i} style={{ margin: "15px" }}>
-            <Card.Header style={{ display: "flex" }}>
-              <div
-                style={{
-                  color: "black",
-                  textDecoration: "none",
-                  flex: 1,
-                  cursor: "pointer",
-                  alignItems: "flex-start",
-                  justifyContent: "flex-start",
-                }}
-              >
-                {/* {note.title} */}
-                <Badge style={{ padding: "10px", marginTop: "12px" }} bg="info">
-                  {note.title}
-                </Badge>
-              </div>
-
-              <div>
-                <Button style={{ margin: "10px" }} variant="primary">
-                  <Link
-                    to="/mynotes/${note._id}"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Edit
-                  </Link>
-                </Button>
-                <Button
-                  style={{ margin: "10px" }}
-                  variant="danger"
-                  onClick={() => deleteHandler(note._id)}
+          <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                {notes.title}
+                <div
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignItems: "flex-start",
+                    justifyContent: "flex-start",
+                  }}
                 >
-                  <Link
-                    to=""
-                    style={{ textDecoration: "none", color: "white" }}
+                  <Badge
+                    style={{ padding: "10px", marginTop: "12px" }}
+                    bg="info"
                   >
-                    Delete
-                  </Link>
-                </Button>
-              </div>
-            </Card.Header>
+                    {note.title}
+                  </Badge>
+                </div>
 
-            <Card.Body>
-              <Badge bg="success">{`Category -> ${note.category}`}</Badge>
-              <blockquote className="blockquote mb-0">
-                <p>{note.content}</p>
-                <footer className="blockquote-footer">
-                  Note Created at <cite title="Source Title"></cite>
-                </footer>
-              </blockquote>
-            </Card.Body>
-          </Card>
+                <div>
+                  <Button style={{ margin: "10px" }} variant="primary">
+                    <Link
+                      to="/mynotes/${note._id}"
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      Edit
+                    </Link>
+                  </Button>
+                  <Button
+                    style={{ margin: "10px" }}
+                    variant="danger"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    <Link
+                      to=""
+                      style={{ textDecoration: "none", color: "white" }}
+                    >
+                      Delete
+                    </Link>
+                  </Button>
+                </div>
+              </Accordion.Header>
+
+              <Accordion.Body>
+                <Badge bg="success">{`Category -> ${note.category}`}</Badge>
+                <blockquote className="blockquote mb-0">
+                  <p>{note.content}</p>
+                  <footer className="blockquote-footer">
+                    Note Created at <cite title="Source Title"></cite>
+                  </footer>
+                </blockquote>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         ))}
       </MainScreen>
     </>
